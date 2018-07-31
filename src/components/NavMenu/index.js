a1a1cc361db27352cfb1e3c90d6e58f9ca7fc6ca
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2018-07-31 20:59:15 -0300

    Capítulo 7 EXERCÍCIO: Importando o restante da estrutura do projeto

--- src/components/NavMenu/index.js
+++ src/components/NavMenu/index.js
@@ -1,47 +1,30 @@
-import React, { Component } from 'react';
-import './navMenu.css';
+import React, { Component } from 'react'
+import './navMenu.css'
 
 class NavMenu extends Component {
-
     render() {
-
         return (
             <nav className="navMenu">
-            <ul className="navMenu__lista">
-
-              <li className="navMenu__item">
-                <a className="navMenu__link">
-                  Bem vindo(a): <br/>
-                  <strong>{ this.props.usuario }</strong>
-                </a>
-              </li>
-
-              <li className="navMenu__item">
-                <a className="navMenu__link">
-                  Página Inicial
-                </a>
-              </li>
-
-
-              <li className="navMenu__item">
-                <a className="navMenu__link" href="">
-                  Hashtags
-                </a>
-              </li>
-
-              <li className="navMenu__item">
-                <a className="navMenu__link" href="">
-                  Logout
-                </a>
-              </li>
-
-
-
-            </ul>
-          </nav>
-        );
-
+                <ul className="navMenu__lista">
+                <li className="navMenu__item">
+                    <a className="navMenu__link">
+                        Bem vindo(a): <br />
+                        <strong>{ this.props.usuario }</strong>
+                    </a>
+                </li>
+                <li className="navMenu__item">
+                    <a className="navMenu__link" href="">Página Inicial</a>
+                </li>
+                <li className="navMenu__item">
+                    <a className="navMenu__link">Hashtags</a>
+                </li>
+                <li className="navMenu__item">
+                    <a className="navMenu__link">Logout</a>
+                </li>
+                </ul>
+            </nav>
+        )
     }
 }
 
-export default NavMenu;
+export default NavMenu
